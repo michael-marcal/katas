2584fbc53fcd03557380cecf63f6024fb8d7e938
michael-marcal <mamjrdotcom@gmail.com>
2022-03-05 20:14:57 -0500

    Adding test_Pairs for MappingTest

--- modern-java-in-action-katas/src/test/java/com/michaelmarcal/modernjavainaction/chapter5/MappingTest.java
+++ modern-java-in-action-katas/src/test/java/com/michaelmarcal/modernjavainaction/chapter5/MappingTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This Kata has been written around Quiz 5.2 of Modern Java in Action.
@@ -16,7 +18,7 @@ public class MappingTest {
 
     @Test
     @Tag("KATA")
-    public void test_Mapping() {
+    public void test_Squares() {
         List<Integer> numbers = Arrays.asList( 1, 2, 3, 4, 5 );
 
         /*
@@ -26,5 +28,23 @@ public class MappingTest {
 
         List<Integer> expected = Arrays.asList( 1, 4, 9, 16, 25 );
         Assertions.assertEquals( expected, squares );
+    }
+
+    @Test
+    @Tag("KATA")
+    public void test_Pairs() {
+        List<Integer> list1 = Arrays.asList( 1, 2, 3 );
+        List<Integer> list2 = Arrays.asList( 3, 4 );
+
+        /*
+            Given 2 lists of numbers, return a list of all pairs of numbers.  A pair is 1 number from list1, and
+            another number from list2.  Order doesn't matter, meaning [1, 3] is the same as [3, 1].  The pairs can
+            be represented as an array of 2 elements.
+         */
+        List<Integer[]> result = new ArrayList<>();
+
+        List<Integer[]> expected = Arrays.asList(new Integer[]{1,3}, new Integer[] {1,4}, new Integer[] {2,3},
+                new Integer[] {2,4}, new Integer[] {3,3}, new Integer[] {3,4});
+        Assertions.assertEquals(Set.copyOf(expected), Set.copyOf(result));
     }
 }
